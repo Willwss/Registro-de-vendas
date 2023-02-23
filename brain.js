3d2426fc37f0b9535e0888ece37aaa2b9e503d5d
William <will_silva_2011@hotmail.com>
2023-02-23 15:24:20 -0300

    Add nome personalizado para o arquivo baixado

--- brain.js
+++ brain.js
@@ -1,52 +1,53 @@
 const toggleDarkModeButton = document.querySelector('#toggle-dark-mode');
 toggleDarkModeButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+document.body.classList.toggle('dark-mode');
 });
 
 class RegistroProduto {
-  constructor() {
-    this.produtos = [];
-  }
+constructor() {
+  this.produtos = [];
+}
 
-  registrarProduto() {
-    // Obter os valores do formulário
-    const nomeCliente = document.querySelector('#nomeCliente').value;
-    const cpf = document.querySelector('#cpf').value;
-    const telefone = document.querySelector('#telefone').value;
-    const dataCompra = document.querySelector('#dataCompra').value;
-    const nomeProduto = document.querySelector('#nomeProduto').value;
-    const validadeProduto = document.querySelector('#validadeProduto').value;
-    const valorProduto = document.querySelector('#valorProduto').value;
-    const numParcelas = document.querySelector('#numParcelas').value;
+ registrarProduto() {
+  // Pega valores do formulário
+  const nomeCliente = document.querySelector('#nomeCliente').value;
+  const cpf = document.querySelector('#cpf').value;
+  const telefone = document.querySelector('#telefone').value;
+  const dataCompra = document.querySelector('#dataCompra').value;
+  const nomeProduto = document.querySelector('#nomeProduto').value;
+  const validadeProduto = document.querySelector('#validadeProduto').value;
+  const valorProduto = document.querySelector('#valorProduto').value;
+  const numParcelas = document.querySelector('#numParcelas').value;
 
-    // Obter os nomes dos campos do formulário
-    const formFields = document.querySelectorAll('#form input');
-    const fieldNames = Array.from(formFields).map(field => field.name);
+  // Pega nomes dos campos do formulário
+  const formFields = document.querySelectorAll('#form input');
+  const fieldNames = Array.from(formFields).map(field => field.name);
 
-    // Criar uma linha com os títulos dos campos
-    const titleRow = `${fieldNames.join(',')}\n`;
+  // Cria uma linha com os títulos dos campos
+  const titleRow = `${fieldNames.join(',')}\n`;
 
-    // Adicionar a linha de títulos ao arquivo CSV
-    let csvData = `data:text/csv;charset=utf-8,${titleRow}`;
+  // Adiciona a linha de títulos ao arquivo CSV
+  let csvData = `data:text/csv;charset=utf-8,${titleRow}`;
 
-    // Obter o valor de cada campo e criar uma linha no formato CSV com as informações do produto
-    const csvRow = `${nomeCliente},${cpf},${telefone},${dataCompra},${nomeProduto},${validadeProduto},${valorProduto},${numParcelas}\n`;
+  // Obter o valor de cada campo e criar uma linha no formato CSV com as informações do produto
+  const csvRow = `${nomeCliente},${cpf},${telefone},${dataCompra},${nomeProduto},${validadeProduto},${valorProduto},${numParcelas}\n`;
 
-    // Adicionar a linha de dados ao arquivo CSV
+    // Adiciona a linha de dados ao arquivo CSV
     csvData += `${csvRow}`;
 
-    // Criar um elemento de link de download
+    // Cria um elemento de link de download
     const link = document.createElement('a');
+    const nomeArquivo = document.querySelector('#nomeArquivo').value;
     link.setAttribute('href', encodeURI(csvData));
-    link.setAttribute('download', 'produtos.csv');
+    link.setAttribute('download', `${nomeArquivo}.csv`);
 
-    // Adicionar o elemento ao corpo do documento
+    // Adiciona o elemento ao corpo do documento
     document.body.appendChild(link);
 
-    // Clicar no elemento para iniciar o download
+    // Clica no elemento para iniciar o download
     link.click();
 
-    // Criar um objeto com as informações do produto
+    // Cria um objeto com as informações do produto
     const produto = {
       nomeCliente,
       cpf,
@@ -58,31 +59,31 @@ class RegistroProduto {
       numParcelas
     };
 
-    // Adicionar o produto ao array de produtos
+    // Adiciona o produto ao array de produtos
     this.produtos.push(produto);
 
-    // Exibir mensagem de sucesso
+    // Exibe mensagem de sucesso
     const successMessage = document.createElement('div');
     successMessage.classList.add('success-message');
     successMessage.innerText = 'Produto registrado com sucesso!';
     document.body.appendChild(successMessage);
 
-    // Limpar os campos do formulário
+    // Limpa os campos do formulário
     document.querySelector('#form').reset();
 
-    // Esconder a mensagem de sucesso após 3 segundos
+    // Esconde a mensagem de sucesso após 3 segundos
     setTimeout(() => {
       successMessage.remove();
     }, 3000);
   }
 }
 
-// Instanciar um objeto de RegistroProduto
+// Instancia um objeto de RegistroProduto
 const registroProduto = new RegistroProduto();
 
-// Adicionar um listener de evento para o botão de registro
+// Adiciona um listener de evento para o botão de registro
 document.querySelector('#btn-registrar').addEventListener('click', (event) => {
   event.preventDefault();
-  // Chamar o método de registro do objeto
+  // Chama o método de registro do objeto
   registroProduto.registrarProduto();
 });
